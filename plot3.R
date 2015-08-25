24f0273e22a71be18fc9729e57c70304dd25b333
RupaNair2 <rupa.v.nair@monsanto.com>
2015-08-25 23:43:58 +0530

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,17 +1,22 @@
-datafile <- "household_power_consumption.txt" 
-data <- read.table(datafile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".") 
-subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,] 
 
 
-datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S")  
+#read file
+file <- "household_power_consumption.txt" 
+#read data
+data_1<- read.table(file, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".") 
+# subset data
+datasubset <- data_1[data_1$Date %in% c("1/2/2007","2/2/2007") ,] 
+
+#strp based date-time
+date_time <- strptime(paste(datasubset$Date, datasubset$Time, sep=" "), "%d/%m/%Y %H:%M:%S")  
  
-submetering1 <- as.numeric(subSetData$Sub_metering_1) 
-submetering2 <- as.numeric(subSetData$Sub_metering_2) 
-submetering3 <- as.numeric(subSetData$Sub_metering_3) 
-
-plot(datetime, submetering1, type="l", ylab="Energy Submetering", xlab="") 
-lines(datetime, submetering2, type="l", col="red") 
-lines(datetime, submetering3, type="l", col="blue") 
+Sub_metering_1 <- as.numeric(datasubset$Sub_metering_1) 
+Sub_metering_2 <- as.numeric(datasubset$Sub_metering_2) 
+Sub_metering_3 <- as.numeric(datasubset$Sub_metering_3) 
+#Plot
+plot(date_time, Sub_metering_1, type="l", ylab="Energy Submetering", xlab="") 
+lines(date_time, Sub_metering_2, type="l", col="red") 
+lines(date_time, Sub_metering_3, type="l", col="blue") 
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue")) 
 
 dev.copy(png, file="plot3.png", height=480, width=480) 
